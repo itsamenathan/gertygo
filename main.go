6eb22bb8546ca911244ddaa568cef84f11aff606
Nathan Warner <nathan@frcv.net>
2023-09-06 18:55:36 -0600

    All the code

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"main/lib/discord"
+	"main/lib/finnhub"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	discord.Init(os.Getenv("DISCORD_TOKEN"))
+	finnhub.Init(os.Getenv("FINNHUB_TOKEN"))
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running...  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	discord.Close()
+}
